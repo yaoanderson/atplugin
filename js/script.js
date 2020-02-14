86fd6cf7405d40f29f028e96ff2a1bd421dc5aac
joanna <joanna@joannadeMacBook-Pro.local>
2020-02-14 10:41:29 +0800

    release v0.0.8

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 function randomString(len) {
     len = len || 32;
     var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';    /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
@@ -58,31 +59,31 @@ window.onload = function() {
         }
 
         var value = "";
-        // var td = event.target.tagName === "TD"? $(event.target): null;
-        // if (td === null) {
-        //     td = $(event.target).parents("td").length !== 0? $(event.target).parents("td").eq(0): null;
-        // }
-        // if (td !== null) {
-        //     var dom = prompt("Please choose element: [0 is 'table', 1 is 'current' (default)]", 1);
-        //     if (dom === "0") {
-        //         var rowNum = parseInt(td.parent().parent().find("tr").index(td.parent()[0]))+1;
-        //         var colNum = parseInt(td.index());
-        //
-        //         var preStr = rowNum + "th";
-        //         var postStr = (colNum+1) + "th";
-        //         if ($(event.target).parents("table").eq(0).has("thead").length !== 0) {
-        //             postStr = "(" + $(event.target).parents("table").find("thead>tr>th:eq(" + colNum + ")").text() + ")";
-        //         }
-        //         value = "Row " + preStr + "| Col " + postStr + "| Value (" + $(event.target).text() + ")";
-        //
-        //         const table = td.parents("table").eq(0)[0];
-        //         result = w.getUniqueId(table);
-        //         tagName = table.tagName;
-        //
-        //     }
-        //
-        //     control_key = false;
-        // }
+        var td = event.target.tagName === "TD"? $(event.target): null;
+        if (td === null) {
+            td = $(event.target).parents("td").length !== 0? $(event.target).parents("td").eq(0): null;
+        }
+        if (td !== null) {
+            var dom = prompt("Please choose element: [0 is 'table', 1 is 'current' (default)], if need to get table data, please choose 0.", 1);
+            if (dom === "0") {
+                var rowNum = parseInt(td.parent().parent().find("tr").index(td.parent()[0]))+1;
+                var colNum = parseInt(td.index());
+
+                var preStr = rowNum + "th";
+                var postStr = (colNum+1) + "th";
+                if ($(event.target).parents("table").eq(0).has("thead").length !== 0) {
+                    postStr = "(" + $(event.target).parents("table").find("thead>tr>th:eq(" + colNum + ")").text() + ")";
+                }
+                value = "Row " + preStr + "| Col " + postStr + "| Value (" + $(event.target).text() + ")";
+
+                const table = td.parents("table").eq(0)[0];
+                result = w.getUniqueId(table);
+                tagName = table.tagName;
+
+            }
+
+            control_key = false;
+        }
 
         chrome.runtime.sendMessage({
             status: 1,
